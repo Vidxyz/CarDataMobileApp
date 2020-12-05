443823671d547030440f5e6271bf96bedb485041
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2020-12-05 15:59:13 -0500

    Updated selected filters view with attributes and values as well as button to clear

--- lib/src/views/advanced_search/selected_filters.dart
+++ lib/src/views/advanced_search/selected_filters.dart
@@ -64,9 +64,65 @@ class _SelectedFilters extends State<SelectedFilters> {
 
   // Make it a stateless widget? Also, figure out line wrap issue with years, and why that is the case
   Widget displaySelectedAttributeValues(String attributeName, List<String> attributeValues) {
-    return Text(
-      "$attributeName : ${attributeValues.join(",")}",
-      style: TextStyle(fontSize: 15, color: Colors.tealAccent),
+    return Container(
+      padding: EdgeInsets.only(bottom: 2.5),
+      child: Wrap(
+        direction: Axis.horizontal,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 5),
+            child: Text(
+              attributeName,
+              style: TextStyle(
+                color: Colors.redAccent[200],
+                fontSize: 15,
+                fontWeight: FontWeight.bold
+              )
+            ),
+          ),
+          // Gap()
+        ]
+          + _displayAttributeValues(attributeValues),
+      ),
     );
+  }
+
+  List<Widget> _displayAttributeValues(List<String> attributeValues) {
+    return attributeValues.map((value) => Stack(
+      children: [
+        Container(
+        padding: EdgeInsets.only(right: 15, left: 5),
+        decoration: BoxDecoration(
+          color: Colors.teal[700],
+          border: Border.all(
+            color: Colors.teal[700],
+            width: 1
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(10))
+        ),
+        child: Text(
+          value,
+          style: TextStyle(fontSize: 15, color: Colors.white),
+          ),
+        ),
+        Positioned(
+          right: 5.0,
+          top: 1,
+          child: GestureDetector(
+            onTap: (){
+              print("Clear is tapped");
+            },
+            child: Align(
+              alignment: Alignment.topRight,
+              child: CircleAvatar(
+                radius: 5.0,
+                backgroundColor: Colors.red,
+                child: Icon(Icons.close, color: Colors.white, size: 10,),
+              ),
+            ),
+          ),
+        ),
+    ]
+    )).toList();
   }
 }
